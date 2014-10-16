cdc141a606a883afa745f72f24b049f427ff2138
americanpezza <mario.beccia@gmail.com>
2014-10-15 21:56:35 -0400

    Removed the dependency from sqlite3. Request user password to encrypt/decrypt, and generate keys form it instead of using random generated ones.

---
+++ src/crypto.py
@@ -0,0 +1,43 @@
+import os,  aes
+from getpass import getpass
+from settings import APP_PATH, CONFIG_PATH, CONFIG_DB, config
+from pbkdf2 import PBKDF2
+
+
+crypticle = None;
+
+def getCrypticle(pwd,  salt):
+    global crypticle
+    
+    if crypticle is None:
+        key = PBKDF2(pwd, salt).read(48).encode("base64").replace("\n", "")
+        crypt = aes.Crypticle(key)
+        crypticle = crypt
+    
+    return crypticle
+
+def saveConfiguration(output_path,  pwd = None):
+    password = pwd
+    if password is None:
+        password = getpass('Enter your master password: ')
+
+    salt = os.urandom(8)
+    crypt = getCrypticle(password,  salt)
+    encrypted = crypt.dumps(config)
+    
+    with open(output_path, 'wb') as f:
+        f.write(salt + encrypted)
+    
+def loadConfiguration(input_path,  pwd = None):
+    password = pwd
+    if password is None:
+        password = getpass('Enter your master password: ')
+
+    with open(input_path, 'rb') as f:
+        salt = f.read(8)
+        encrypted_text = f.read()
+
+    crypt = getCrypticle(password,  salt)
+    config = crypt.loads(encrypted_text)
+    
+    return config,  crypt
